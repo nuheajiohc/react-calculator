6c78e7af4c15c37adcf614cddbdd552e721e3302
nuheajiohc <jaehun341012@gmail.com>
2023-02-13 16:29:16 +0900

    feat: 기능 구현을 위한 버튼 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="calculator-result">0</div>
+        <div className="digit-wrapper">
+          <button className="digit">9</button>
+          <button className="digit">8</button>
+          <button className="digit">7</button>
+          <button className="digit">6</button>
+          <button className="digit">5</button>
+          <button className="digit">4</button>
+          <button className="digit">3</button>
+          <button className="digit">2</button>
+          <button className="digit">1</button>
+          <button className="digit">0</button>
+        </div>
+        <div className="modifier-wrapper">
+          <button className="modifier">AC</button>
+        </div>
+        <div className="operations-wrapper">
+          <button className="operation">/</button>
+          <button className="operation">X</button>
+          <button className="operation">-</button>
+          <button className="operation">+</button>
+          <button className="operation">=</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
