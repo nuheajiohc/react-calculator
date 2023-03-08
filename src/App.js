ad51fb8bd54558e9ee257b64e8038f52abc9345a
nuheajiohc <jaehun341012@gmail.com>
2023-03-08 16:44:27 +0900

    feat: 기본적인 계산기 모양 구성 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import { useState } from "react";
+import classes from "./App.module.css";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <div className="App"></div>;
+  const calcButtonList = [
+    "AC",
+    "+/-",
+    "%",
+    "/",
+    7,
+    8,
+    9,
+    "X",
+    4,
+    5,
+    6,
+    "-",
+    1,
+    2,
+    3,
+    "+",
+    0,
+    ".",
+    "=",
+  ];
+
+  const buttons = calcButtonList.map(btn => <Button>{btn}</Button>);
+
+  return (
+    <div className={classes.App}>
+      <div className={classes["calc-container"]}>
+        <div className={classes["screens-container"]}>
+          <input />
+          <input />
+        </div>
+        <div className={classes["buttons-container"]}>{buttons}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
