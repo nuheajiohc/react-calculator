121d9f08f7f7b235b5f7e5e4124fa53fdfdab8ac
nuheajiohc <jaehun341012@gmail.com>
2023-03-09 11:49:43 +0900

    feat: 입력한 피연산자 화면에 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
+import { useState } from "react";
 import classes from "./App.module.css";
-import Button from "./components/Button/Button";
+// import Button from "./components/Button/Button";
 
 function App() {
-  const calcButtonList = [
-    "AC",
-    "+/-",
-    "%",
-    "/",
-    7,
-    8,
-    9,
-    "X",
-    4,
-    5,
-    6,
-    "-",
-    1,
-    2,
-    3,
-    "+",
-    0,
-    ".",
-    "=",
-  ];
+  const [enteredNumber, setEnteredNumber] = useState("0");
+  const [displayNumber, setDisplayNumber] = useState("0");
 
-  const buttons = calcButtonList.map(btn => <Button>{btn}</Button>);
+  const numberHandler = event => {
+    const newEnteredNumber = Number(enteredNumber + event.target.innerText);
+    setEnteredNumber(newEnteredNumber.toString());
+    setDisplayNumber(newEnteredNumber.toLocaleString());
+  };
+
+  const clearHandler = () => {
+    setEnteredNumber("0");
+    setDisplayNumber("0");
+  };
+
+  const toggleSignHandler = () => {
+    const convertNumber = -Number(enteredNumber);
+    setEnteredNumber(convertNumber.toString());
+    setDisplayNumber(convertNumber.toLocaleString());
+  };
 
   return (
     <div className={classes.App}>
       <div className={classes["calc-container"]}>
         <div className={classes["screens-container"]}>
-          <input />
-          <input />
+          <div></div>
+          <div>{displayNumber}</div>
         </div>
-        <div className={classes["buttons-container"]}>{buttons}</div>
+        <div className={classes["buttons-container"]}>
+          <button onClick={clearHandler}>AC</button>
+          <button onClick={toggleSignHandler}>+/-</button>
+          <button>%</button>
+          <button>/</button>
+          <button onClick={numberHandler}>7</button>
+          <button onClick={numberHandler}>8</button>
+          <button onClick={numberHandler}>9</button>
+          <button onClick={numberHandler}>X</button>
+          <button onClick={numberHandler}>4</button>
+          <button onClick={numberHandler}>5</button>
+          <button onClick={numberHandler}>6</button>
+          <button>-</button>
+          <button onClick={numberHandler}>1</button>
+          <button onClick={numberHandler}>2</button>
+          <button onClick={numberHandler}>3</button>
+          <button>+</button>
+          <button onClick={numberHandler}>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </div>
     </div>
   );
