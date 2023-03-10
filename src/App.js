8da46419c94388a3b1192e9e2a25cc157d62fee7
nuheajiohc <jaehun341012@gmail.com>
2023-03-10 22:21:50 +0900

    feat: 퍼센트 기능 구현

--- src/App.js
+++ src/App.js
@@ -37,14 +37,40 @@ function App() {
     setEnteredNumber(newEnteredNumber.toString());
   };
 
-  const convertToPercentHandler = () => {};
+  // indexOf() === -1 일 경우 맨 뒤에 점이 있다고 가정
+  // 점앞의 숫자가 한개 1.1>> 0.011 점 떼고 앞에 0두개 붙이고, 첫번째뒤에 점
+  //  두개  10.1  >> 0.101   점 떼고 앞에 0 한개 붙이고, 첫번째 뒤에 점
+  //  세개 100.1 >> 1.001    점떼고 첫번째 숫자 뒤에 점
 
-  let displayNumber = Number(enteredNumber).toLocaleString(undefined, {
-    maximumFractionDigits: 8,
-  });
-  if (enteredNumber.at(-1) === ".") {
-    displayNumber += ".";
-  }
+  const convertToPercentHandler = () => {
+    if (enteredNumber === "0") return;
+    if (enteredNumber === "0.") {
+      setEnteredNumber("0");
+      return;
+    }
+    let convertToPercentNumber = enteredNumber;
+    let enteredNumberSplit = enteredNumber.split(".");
+    if (enteredNumberSplit[0].length === 1) {
+      convertToPercentNumber = `0.0${enteredNumberSplit[0]}`;
+    } else if (enteredNumberSplit[0].length === 2) {
+      convertToPercentNumber = `0.${enteredNumberSplit[0]}`;
+    } else {
+      convertToPercentNumber =
+        enteredNumberSplit[0].slice(0, -2) +
+        "." +
+        enteredNumberSplit[0].slice(-2);
+    }
+    if (enteredNumberSplit[1] !== undefined) {
+      convertToPercentNumber += enteredNumberSplit[1];
+    }
+    if (convertToPercentNumber.replace(/[-.]/g, "").length > 9) return;
+    setEnteredNumber(convertToPercentNumber);
+  };
+
+  const [integerPart, decimalPart] = enteredNumber.split(".");
+  let displayNumber = Number(integerPart).toLocaleString();
+  if (enteredNumber.split(".").length === 2) displayNumber += ".";
+  if (decimalPart !== undefined) displayNumber += decimalPart;
 
   return (
     <div className={classes.App}>
